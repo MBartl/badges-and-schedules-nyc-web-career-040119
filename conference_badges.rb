177b663a310503c50ca9c83ef8e9673a9cd19dad
Matthew Bartl <mattbartl@gmail.com>
2019-03-19 23:06:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,10 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  batch_messages = []
   attendees.each do |name|
-    return "Hello, my name is #{name}"
+    batch_messages.push("Hello, my name is #{name}.")
   end
+  return batch_messages
 end
+
